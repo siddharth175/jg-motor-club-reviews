74486fed5de615bfbac37c7103fab215a9808a0e
Siddharth Singh <sid996675@gmail.com>
2026-01-29 22:07:21 -0500

    Added new google map link

--- src/lib/google-url.ts
+++ src/lib/google-url.ts
@@ -1,6 +1,6 @@
-// Derived from user provided link: https://www.google.com/maps/place/J+%26+G+Motor+Club/@40.7422375,-74.3895589,17z/data=!3m1!4b1!4m6!3m5!1s0x89c3af071eb44e91:0x423a622e1b5d1a3d!8m2!3d40.7422375!4d-74.386984
-export const GOOGLE_PLACE_ID = "0x89c3af071eb44e91:0x423a622e1b5d1a3d"; // CID
+// Derived from user provided link CID: 0x423a622e1b5d1a3d -> 4772234705319565885
+export const GOOGLE_PLACE_ID = "4772234705319565885";
 
 export const getGoogleReviewUrl = () => {
-    return "https://www.google.com/maps/place/J+%26+G+Motor+Club/@40.7422375,-74.3895589,17z/data=!3m1!4b1!4m6!3m5!1s0x89c3af071eb44e91:0x423a622e1b5d1a3d";
+    return `https://maps.google.com/?cid=${GOOGLE_PLACE_ID}`;
 };
