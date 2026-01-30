08d28f1f7004d2998aab96caec2ffda4ac6546d8
Siddharth Singh <sid996675@gmail.com>
2026-01-29 21:35:51 -0500

    Fix review fetch logic

--- src/lib/google-url.ts
+++ src/lib/google-url.ts
@@ -1,5 +1,6 @@
-export const GOOGLE_PLACE_ID = "ChIJt7j_M9BTwokRfTUfHwFKRUQ";
+// Derived from user provided link: https://www.google.com/maps/place/J+%26+G+Motor+Club/@40.7422375,-74.3895589,17z/data=!3m1!4b1!4m6!3m5!1s0x89c3af071eb44e91:0x423a622e1b5d1a3d!8m2!3d40.7422375!4d-74.386984
+export const GOOGLE_PLACE_ID = "0x89c3af071eb44e91:0x423a622e1b5d1a3d"; // CID
 
 export const getGoogleReviewUrl = () => {
-    return `https://search.google.com/local/writereview?placeid=${GOOGLE_PLACE_ID}`;
+    return "https://www.google.com/maps/place/J+%26+G+Motor+Club/@40.7422375,-74.3895589,17z/data=!3m1!4b1!4m6!3m5!1s0x89c3af071eb44e91:0x423a622e1b5d1a3d";
 };
